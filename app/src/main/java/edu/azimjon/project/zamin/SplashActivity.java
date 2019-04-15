b9ef6549ad64fa811ef77c5cd36edad1bf6c49ed
Azimjon98 <998916379677artur@gmail.com>
2019-04-15 12:26:36 +0500

    implementing splash screen

--- app/src/main/java/edu/azimjon/project/zamin/SplashActivity.java
+++ app/src/main/java/edu/azimjon/project/zamin/SplashActivity.java
@@ -31,7 +31,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
         Locale newLocale = new Locale(MySettings.getInstance().getLocale());
         Locale.setDefault(newLocale);
